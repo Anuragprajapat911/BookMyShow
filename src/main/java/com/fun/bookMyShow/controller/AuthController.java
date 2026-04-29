8bdf43a6660df9a1dd4dc375344d27968fe17020
Anurag Prajpat <anurag.93997@gmail.com>
2026-04-29 11:19:30 +0530

    update

--- src/main/java/com/fun/bookMyShow/controller/AuthController.java
+++ src/main/java/com/fun/bookMyShow/controller/AuthController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://book-my-show-frontend-434s.vercel.app/")
 
 @RestController
 @RequestMapping("/api/auth")
--- src/main/java/com/fun/bookMyShow/security/SecurityConfig.java
+++ src/main/java/com/fun/bookMyShow/security/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://book-my-show-frontend-434s.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
